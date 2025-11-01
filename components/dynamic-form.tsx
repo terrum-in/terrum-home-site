0531ef0c25e79a6156e374cb9a10906e83308841
Abhishek A N <me@abhishekan.dev>
2025-11-01 14:58:40 +0530

    Add razorpay signature to request body

--- components/dynamic-form.tsx
+++ components/dynamic-form.tsx
@@ -91,6 +91,7 @@ const DynamicForm: React.FC<{
                 body: JSON.stringify({
                   razorpay_payment_id: response.razorpay_payment_id,
                   razorpay_order_id: response.razorpay_order_id,
+                  razorpay_signature: response.razorpay_signature,
                 }),
               }
             );
